86a1e996adce92cc76f0ff59264a9dbe1954a47f
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2019-01-17 21:54:47 +0000

    Registers Car~ models with admin site

--- tinycar/rentals/admin.py
+++ tinycar/rentals/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from rentals.models import CarMake, CarModel, Car
 
+admin.site.register(CarMake)
+admin.site.register(CarModel)
+admin.site.register(Car)
 # Register your models here.
