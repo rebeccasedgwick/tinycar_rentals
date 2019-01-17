0d9cf3ff89a048429518c728369169fd7c4b957c
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2019-01-17 21:30:51 +0000

    Adds basic model info for cars (make, model, indiv. car)

--- tinycar/rentals/models.py
+++ tinycar/rentals/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class CarMake(models.Model):
+    """
+    Model representing the make (manufacturer) of a car
+    """
+    name = models.CharField('Car make', blank=False)
+
+
+class CarModel(models.Model):
+    """
+    Model representing the model of a car
+    """
+    name = models.CharField('Car model', blank=False)
+    make = models.ForeignKey('CarMake', blank=False)
+
+
+class Car(models.Model):
+    """
+    Model representing an individual car
+    """
+    model = models.ForeignKey('CarModel', blank=False)
+
+    registration = models.CharField(
+        'Registration number',
+        max_length=7,
+        help_text="Enter car's registration number (no spaces)"
+    )
+
+    vin = models.CharField(
+        'VIN',
+        max_length=17,
+        help_text="Vehicle identification number"
+    )
